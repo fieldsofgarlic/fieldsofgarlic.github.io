66b8a75cc8a71b939046dea03e00727b6cd242a9
Dave Cash <fieldsofgarlic@gmail.com>
2018-10-30 10:06:57 +0000

    finished eqjs/chapter3.js

--- eqjs/chapter3.js
+++ eqjs/chapter3.js
@@ -1,5 +1,9 @@
 // Write a function min that takes two arguments and returns their minimum.
 
+let min = ( n1, n2 ) => [ n1, n2 ].sort( ( a, b ) => a - b )[ 0 ];
+console.log( min( 0, 10 ) );
+console.log( min( 0, -10 ) );
+
 /* Zero is even.  One is odd.  For any other number N, its evenness is the
  * same as N - 2.
  *
@@ -11,14 +15,47 @@
  * way to fix this?
  */
  
+function isEven( n ) {
+    switch ( n ) {
+        case 0: return true;
+        case 1: return false;
+        default: return isEven( n - 2 );
+    }
+}
+
+console.log( isEven( 50 ) );
+console.log( isEven( 75 ) );
+
+function isEven( n ) {
+    switch ( Math.abs( n ) ) {
+        case 0: return true;
+        case 1: return false;
+        default: return isEven( Math.abs( n ) - 2 );
+    }
+}
+
+console.log( isEven( -50 ) );
+console.log( isEven( -75 ) );
+
 /* Write a function countBs that takes a string as its only argument and
  * returns a number that indicates how many uppercase “B” characters there
  * are in the string.
  */
+
+function countBs( str ) {
+    return [ ...str ].filter( c => c === 'B' ).length;
+}
 
 /* Next, write a function called countChar that behaves like countBs, except
  * it takes a second argument that indicates the character that is to be
  * counted (rather than counting only uppercase “B” characters).
  */ 
 
+function countChar( str, char ) {
+    return [ ...str ].filter( c => c === char ).length;
+}
+
 // Rewrite countBs to make use of this new function.
+function countBs2( str ) {
+    return countChar( str, 'B' );
+}
