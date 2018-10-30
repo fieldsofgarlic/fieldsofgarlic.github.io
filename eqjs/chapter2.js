fd8d19de3ac21d501f66724d38c13171aa736d49
Dave Cash <fieldsofgarlic@gmail.com>
2018-10-30 02:37:01 +0000

    finished eqjs/chapter2.js

--- eqjs/chapter2.js
+++ eqjs/chapter2.js
@@ -9,10 +9,46 @@
  * #######
  */
 
-let str = '';
 for ( let i = 0; i < 7; i ++ ) {
-    console.log( str += '#' );
+    console.log( '#'.repeat( i + 1 ) );
 }
+
+/* Write a program that uses console.log to print all the numbers from 1 to
+ * 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead
+ * of the number, and for numbers divisible by 5 (and not 3), print "Buzz"
+ * instead.
+ */
+ 
+[ ...Array(100).keys() ].map(
+    function ( num ) {
+        let out = num + 1;
+        if ( out % 3 === 0 ) {
+            out = 'Fizz';
+        } else if ( out % 5 === 0 ) {
+            out = 'Buzz';
+        }
+        console.log( out );
+    }
+);
+
+/* When you have that working, modify your program to print "FizzBuzz" for
+ * numbers that are divisible by both 3 and 5 (and still print "Fizz" or
+ * "Buzz" for numbers divisible by only one of those).
+ */
+
+[ ...Array(100).keys() ].map(
+    function ( num ) {
+        let out = num + 1;
+        if ( out % 3 === 0 && out % 5 === 0 ) {
+            out = 'FizzBuzz';
+        } else if ( out % 3 === 0 ) {
+            out = 'Fizz';
+        } else if ( out % 5 === 0 ) {
+            out = 'Buzz';
+        }
+        console.log( out );
+    }
+);
 
 /* Write a program that creates a string that represents an 8×8 grid, using newline
  * characters to separate lines. At each position of the grid there is either a
@@ -29,15 +65,26 @@ for ( let i = 0; i < 7; i ++ ) {
  *  # # # #
  * # # # #
  *
+ */
+ 
+let output = '';
+for ( let i = 0; i < 8; i ++ ) {
+    output += i % 2 ? ' #'.repeat( 4 ) : '# '.repeat( 4 );
+    output += "\n";
+}
+console.log( output );
+
+/*
  * When you have a program that generates this pattern, define a binding size = 8
  * and change the program so that it works for any size, outputting a grid of the
  * given width and height.
  */
  
 let bindingSize = 8;
-let output = '';
+output = '';
 for ( let i = 0; i < bindingSize; i ++ ) {
-    output += i % 2 ? ' # # # #' : '# # # # ';
+    output += i % 2 ? ' #'.repeat( bindingSize / 2 ) : '# '.repeat( bindingSize / 2 );
     output += "\n";
 }
 console.log( output );
+
--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -11,20 +11,30 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+animal = {};
+animal.species = 'tardigrade';
+animal[ 'name' ] = 'Cuddles';
+animal.noises = [];
+console.log( animal );
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+noises = [];
+noises[ 0 ] = 'roar';
+noises.push( 'squeak' );
+noises.unshift( 'chomp' );
+noises[ noises.length ] = 'chirp';
+console.log( noises.length );
+console.log( noises[ noises.length - 1 ] );
+console.log( noises );
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+animal[ 'noises' ] = noises;
+animal.noises.splice( 1, 0, 'wheeze' );
+console.log( animal );
 
 /* *******************************************************************
  * Step 4 - Review
@@ -48,14 +58,41 @@
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+animals = [];
+animals.push( animal );
+console.log( animals );
+let duck = { species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] };
+animals.push( duck );
+console.log( duck );
+animals.push(
+    {
+        name: 'Wazuu',
+        species: 'wingding',
+        nosies: [ 'huh?', 'oh', 'hmmm' ]
+    },
+    {
+        name: 'Lo Fye',
+        species: 'hyena',
+        noises: [ 'chortle', 'shriek', 'whoop' ]
+    }
+);
+console.log( animals );
+console.log( animals.length );
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+friends = []; // making this an array because it needs to hold a list
 
+function getRandom ( arr ) {
+    return Math.floor( Math.random() * arr.length );
+}
 
+friends.push( animals[ getRandom( animals ) ].name );
+console.log( friends );
+
+animal.friends = friends;
+console.log( animal );
 
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
